13a5af8b4169304c5b3557c783b7bd2b6b5dc30e
Damian Di Giovanni <demian-dg@hotmail.com>
2024-10-01 19:30:17 -0300

    Se agregan las clase Articulo y clases de sus atributos por agregación

---
+++ Dominio/Articulo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Dominio
+{
+    public class Articulo
+    {
+        public int ID { get; set; }
+
+        [DisplayName("Código")]
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+
+        [DisplayName("Descripción")]
+        public string Descripcion { get; set; }
+        public Marca Marca { get; set; }
+        public float Precio { get; set; }
+
+        [DisplayName("Categoía")]
+        public Categoria Categoria { get; set; }
+        public Imagen Imagen { get; set; }
+
+    }
+}
